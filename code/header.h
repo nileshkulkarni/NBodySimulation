b9c703f60758714cede26f65bf9bffbc2fc3a08e
nileshkulkarni <nileshsatishkulkarni@gmail.com>
2012-10-20 23:51:55 +0530

    edited header file

--- code/header.h
+++ code/header.h
@@ -1,5 +1,29 @@
 
 
 namespace local {
+	class Point{
+		public:
+		float x;
+		float y;
+		
+	};
+	
 
-	class Particle 
+	class Particle{
+		private:
+		float _mass;
+		Point _position;
+		double _velocity;
+		double _acceleration;
+		double _radius;
+
+		public:
+		
+		Particle();
+		Point getPosition();
+		double getVelocity();
+		double getAcceleration();
+		double getRadius();
+
+	};
+
